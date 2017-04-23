f4b65baf15398b91506010b3cdedf3b2e206d745
rockncoder <rockncoder@gmail.com>
2017-04-23 12:36:17 -0700

    Moves routes to its own file.

--- client/index.js
+++ client/index.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './App';
-import {browserHistory, Router, Route} from 'react-router';
-import {Home} from './Home';
-import {About} from './About';
-import {ToDoDemo} from './ToDoDemo';
+import {browserHistory} from 'react-router';
 const mountApp = document.getElementById('root');
 console.log("My key = " + process.env.GAPI_KEY);
 export const GAPI_KEY = process.env.GAPI_KEY;
-
-
-const routes = (
-  <Router>
-    <Route path="/" component={Home}/>
-    <Route path="/about" component={About}/>
-    <Route path="/todo" component={ToDoDemo}/>
-  </Router>
-);
+import routes from "./routes";
 
 const render = Component => {
   ReactDOM.render(
